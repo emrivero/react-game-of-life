0b27c3485b1d7063f928a594fe78d806101f1fbd
emrivero <emiliomartinez@guadaltel.com>
2019-03-04 10:27:50 +0100

    Deploy gh-pages

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
-import { MuiThemeProvider } from '@material-ui/core/styles';
+import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
+import {MuiThemeProvider} from '@material-ui/core/styles';
 import theme from './style/theme';
 import './index.css';
-import { register } from './serviceWorker';
+import {register} from './serviceWorker';
 
 import Main from './pages/Main';
 
-ReactDOM.render(
-  <Router basename="/hcsigc">
-      <MuiThemeProvider theme={theme}>
-        <Switch>
-          <Redirect exact={true} from='/' to='/main'/>
-          <Route path="/main" exact={true} component={Main}/>
-        </Switch>
-      </MuiThemeProvider>
-  </Router>, document.getElementById('root'));
+ReactDOM.render(<Router basename="/react-game-of-life">
+  <MuiThemeProvider theme={theme}>
+    <Switch>
+      <Redirect exact={true} from='/' to='/main'/>
+      <Route path="/main" exact={true} component={Main}/>
+    </Switch>
+  </MuiThemeProvider>
+</Router>, document.getElementById('root'));
 register();
