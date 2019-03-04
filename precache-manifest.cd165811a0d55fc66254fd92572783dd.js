36d72a0c3cd712ac9e9df0e1309374befc8f560a
emrivero <emiliomartinez@guadaltel.com>
2019-03-04 08:59:30 +0100

    Deploy github page

---
+++ precache-manifest.cd165811a0d55fc66254fd92572783dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cbbc15549b1aaf479b7",
+    "url": "/react-game-of-life/static/js/runtime~main.4cbbc155.js"
+  },
+  {
+    "revision": "36fc9dfa8858078e49e8",
+    "url": "/react-game-of-life/static/js/main.36fc9dfa.chunk.js"
+  },
+  {
+    "revision": "197fcf27f9cf328c7d7c",
+    "url": "/react-game-of-life/static/js/2.197fcf27.chunk.js"
+  },
+  {
+    "revision": "36fc9dfa8858078e49e8",
+    "url": "/react-game-of-life/static/css/main.c64ffbe2.chunk.css"
+  },
+  {
+    "revision": "da0b056cb6710188c31dd4171b59f1f5",
+    "url": "/react-game-of-life/index.html"
+  }
+];
