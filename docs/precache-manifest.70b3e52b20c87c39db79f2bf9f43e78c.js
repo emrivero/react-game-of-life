85f21ec3ce8c2a8f6f925447acaaad742eb4368c
Emilio Martinez Rivero <asemeu7@gmail.com>
2019-03-05 22:33:36 +0100

    Se añade carpeta docs

---
+++ docs/precache-manifest.70b3e52b20c87c39db79f2bf9f43e78c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4cbbc15549b1aaf479b7",
+    "url": "/react-game-of-life/static/js/runtime~main.4cbbc155.js"
+  },
+  {
+    "revision": "c623c6e4f682492bfd39",
+    "url": "/react-game-of-life/static/js/main.c623c6e4.chunk.js"
+  },
+  {
+    "revision": "dc7f8a93f65eee066d77",
+    "url": "/react-game-of-life/static/js/2.dc7f8a93.chunk.js"
+  },
+  {
+    "revision": "c623c6e4f682492bfd39",
+    "url": "/react-game-of-life/static/css/main.517ac67c.chunk.css"
+  },
+  {
+    "revision": "322e5788715e7f2c08f6418f6757f864",
+    "url": "/react-game-of-life/index.html"
+  }
+];
